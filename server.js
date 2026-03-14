55ac6b213316ff8304de554aa9c87065a52460b6
ilnautico <fairvia.trading@gmail.com>
2026-03-15 00:40:32 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req,res)=>{
   res.send("FairVia server running");
 });
 
-app.listen(3000,()=>{
+app.post("/generate-report",(req,res)=>{
+
+  const data = req.body;
+
+  const html = `
+  <html>
+  <head>
+  <title>FairVia Report</title>
+  </head>
+  <body>
+  <h1>FairVia Screening Report</h1>
+
+  <p><b>Application:</b> ${data.application}</p>
+  <p><b>Material:</b> ${data.material}</p>
+  <p><b>Bio material:</b> ${data.bio_material}</p>
+  <p><b>Equipment:</b> ${data.equipment}</p>
+
+  </body>
+  </html>
+  `;
+
+  res.send(html);
+
+});
+
+app.listen(process.env.PORT || 3000,()=>{
   console.log("server started");
 });
