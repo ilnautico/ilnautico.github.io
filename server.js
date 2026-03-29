e5ebd91018800a2b037205cd19c4584b71ab6405
ilnautico <fairvia.trading@gmail.com>
2026-03-29 15:07:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -228,8 +228,6 @@ await browser.close();
   
   page.setContent(finalHtml, { waitUntil: "networkidle0" });
 
-  const pdf = await page.pdf({ format: "A4" });
-
   await browser.close();
 
   res.set({
