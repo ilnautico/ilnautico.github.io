37db17b8f3331f4df06397377c8fc1a89b880527
ilnautico <fairvia.trading@gmail.com>
2026-04-02 17:17:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get("/latest-pdf", (req, res) => {
 });
 // =========================
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("🚀 Server running");
 });
 // =========================
