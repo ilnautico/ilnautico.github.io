2eace86df2cdbf56d4d5dad38e0d0baae9a1c3fe
ilnautico <fairvia.trading@gmail.com>
2026-04-05 18:54:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import puppeteer from "puppeteer";
 import fs from "fs";
 
-const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
