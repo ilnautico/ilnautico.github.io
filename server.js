233951c573202c1714cccbd362122ea046ce969c
ilnautico <fairvia.trading@gmail.com>
2026-04-10 04:18:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text({ type: "*/*" }));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
   res.send("SERVER OK");
 });
 
