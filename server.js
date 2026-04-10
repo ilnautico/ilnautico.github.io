48b9fc6ba5ae40629366a0b539ae1ad8745a04a2
ilnautico <fairvia.trading@gmail.com>
2026-04-10 17:39:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -309,8 +309,6 @@ app.get("/latest-pdf", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("🚀 Server running on", PORT);
-  console.log(`Server running on port ${PORT}`);
-});
 const htmlTemplate = `
 <!DOCTYPE html>
 <html lang="en">
