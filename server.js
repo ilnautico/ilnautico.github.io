3547bc2e488a3f3d53c5f9b833e49578f4a98f4f
ilnautico <fairvia.trading@gmail.com>
2026-04-11 23:19:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -86,12 +86,11 @@ function generateOverlay(scoreLeft, scoreRight) {
 
   <!-- 針（今のやつそのまま使う） -->
   <g transform="rotate(${angle} 100 100)">
-    <line x1="100" y1="100" x2="140" y2="65"
-      stroke="#111"
-      stroke-width="2.5"
-      stroke-linecap="round"/>
-    <circle cx="100" cy="100" r="4.5" fill="#111"/>
-  </g>
+  <line x1="100" y1="100" x2="150" y2="60"
+    stroke="#111"
+    stroke-width="2.5"
+    stroke-linecap="round"/>
+</g>
 
 </svg>
   </div>
