54f91a2a8aad0a633116475032d4efee1c82171e
ilnautico <fairvia.trading@gmail.com>
2026-04-12 01:34:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -85,10 +85,12 @@ function generateOverlay(scoreLeft, scoreRight) {
     fill="black" opacity="0.08"/>
 
   <!-- 針（今のやつそのまま使う） -->
-<!-- 針（今のやつそのまま使う） -->
 <g transform="rotate(${angle} 100 100)">
-  <line x1="100" y1="100" x2="55" y2="100"
-  <line x1="100" y1="100" x2="35" y2="100"
+  <line 
+    x1="100" 
+    y1="100" 
+    x2="100" 
+    y2="60"
     stroke="#111"
     stroke-width="2.5"
     stroke-linecap="round"/>
