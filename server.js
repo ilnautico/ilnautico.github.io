c230eab5830cdf05654e3fd92c058d56c0d8effb
ilnautico <fairvia.trading@gmail.com>
2026-04-13 23:21:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -150,7 +150,6 @@ app.post("/generate-report", async (req, res) => {
 /* =========================
    起動
 ========================= */
-const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log("Server running on " + PORT);
