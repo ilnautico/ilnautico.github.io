6bb7e6c10a6aaad0251f5df85e4746e828447fd2
ilnautico <fairvia.trading@gmail.com>
2026-04-14 21:45:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -128,7 +128,8 @@ app.listen(8080, () => {
 // =========================
 // 起動
 // =========================
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("🚀 Server running on", PORT);
 });
