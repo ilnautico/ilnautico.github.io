d961368b9919048cfabbadf83209126a78b2627c
ilnautico <fairvia.trading@gmail.com>
2026-04-16 14:03:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import OpenAI from "openai";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
+const app = express();　
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
