2cee82a6496334710ecfe36b0b00e4b6f6ada533
ilnautico <fairvia.trading@gmail.com>
2026-04-17 13:40:50 +0900

    Update server.js

--- server.js
+++ server.js
@@ -299,7 +299,7 @@ Thermal degradation combined with shear-induced instability acts as the primary 
   // =========================
   // 🔥 overlay（そのまま）
   // =========================
-  base_image: "https://i.imgur.com/8Q0vG6B.png",
+  base_image: "<img src="https://ilnautico.github.io/visual-base.png",
   dynamic_overlay: generateOverlay(score, score)
 
 });
