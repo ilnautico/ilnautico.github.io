cfee1d1235b3c09450aab60dfd325a3c93b72a77
ilnautico <fairvia.trading@gmail.com>
2026-04-17 17:41:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -45,68 +45,44 @@ function generateOverlay(scoreLeft, scoreRight) {
   return `
 <div style="position:absolute; left:0; top:0; width:100%; height:100%; pointer-events:none;">
 
-<img src="https://ilnautico.github.io/visual-base.png"
-  style="
-    position:absolute;
-    left:50%;
-    top:55%;
-    transform:translate(-50%,-50%);
-    width:450px;
-    opacity:0.95;
-  "
-/>
+  <!-- ここから全部HTML -->
 
-  <div style="
-    position:absolute;
-    top:40px;
-    left:50%;
-    transform:translateX(-180px);
-    text-align:center;
-  ">
+  <img src="https://ilnautico.github.io/visual-base.png"
+    style="
+      position:absolute;
+      left:50%;
+      top:55%;
+      transform:translate(-50%,-50%);
+      width:450px;
+      opacity:0.95;
+    "
+  />
+
+  <div style="position:absolute; top:40px; left:50%; transform:translateX(-180px); text-align:center;">
     <div style="font-size:28px; color:#2f3a44;">230°C</div>
     <div style="font-size:16px; color:#5b6770;">${scoreLeft}</div>
   </div>
 
-  <div style="
-    position:absolute;
-    top:40px;
-    left:50%;
-    transform:translateX(180px);
-    text-align:center;
-  ">
+  <div style="position:absolute; top:40px; left:50%; transform:translateX(180px); text-align:center;">
     <div style="font-size:28px; color:#d62c2c;">180°C</div>
     <div style="font-size:16px; color:#d62c2c;">${scoreRight}</div>
   </div>
 
-<svg style="
-  position:absolute;
-  left:50%;
-  bottom:80px;
-  transform:translateX(-70px);
-" width="90" height="35">
-  <path d="M0 18 C15 6, 30 30, 45 18 C60 6, 75 30, 90 18"
-    fill="none"
-    stroke="#4f7c8a"
-    stroke-width="3"/>
-</svg>
+  <svg style="position:absolute; left:50%; bottom:80px; transform:translateX(-70px);" width="90" height="35">
+    <path d="M0 18 C15 6, 30 30, 45 18 C60 6, 75 30, 90 18"
+      fill="none"
+      stroke="#4f7c8a"
+      stroke-width="3"/>
+  </svg>
 
-<svg style="
-  position:absolute;
-  left:50%;
-  bottom:80px;
-  transform:translateX(110px);
-" width="90" height="35">
-  <path d="M0 18 C15 6, 30 30, 45 18 C60 6, 75 30, 90 18"
-    fill="none"
-    stroke="#d62c2c"
-    stroke-width="3"/>
-</svg>
+  <svg style="position:absolute; left:50%; bottom:80px; transform:translateX(110px);" width="90" height="35">
+    <path d="M0 18 C15 6, 30 30, 45 18 C60 6, 75 30, 90 18"
+      fill="none"
+      stroke="#d62c2c"
+      stroke-width="3"/>
+  </svg>
 
-  <svg style="
-    position:absolute;
-    right:60px;
-    bottom:10px;
-  " viewBox="0 0 200 120" width="140" height="90">
+  <svg style="position:absolute; right:60px; bottom:10px;" viewBox="0 0 200 120" width="140" height="90">
 
     <defs>
       <linearGradient id="g">
@@ -133,7 +109,6 @@ function generateOverlay(scoreLeft, scoreRight) {
 </div>
 `;
 }
-
 
   <svg style="
     position:absolute;
