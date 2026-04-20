0148d88f5bd77f1de6d0c1a66549757d2ce458d4
ilnautico <fairvia.trading@gmail.com>
2026-04-20 18:51:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -386,8 +386,14 @@ app.post("/generate-report", async (req, res) => {
     });
 
     const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
+  headless: "new",
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu"
+  ]
+});
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
