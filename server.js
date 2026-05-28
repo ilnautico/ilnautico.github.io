e145211f4d4b98ab8b11910c442322417623fb2b
ilnautico <fairvia.trading@gmail.com>
2026-05-28 17:11:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import puppeteer from "puppeteer";　
+import puppeteer from "puppeteer";　 
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
